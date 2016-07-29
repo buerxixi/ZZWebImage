e6a36a416c4ff378f21df8e1f2bad37c0f00768c
刘家强 <liujiaqiang@outlook.com>
2016-07-29 18:40:32 +0800

    SB页面的基本搭建

---
+++ 7.29网络加载/AppInfo.h
@@ -0,0 +1,13 @@
+//
+//  AppInfo.h
+//  7.29网络加载
+//
+//  Created by 刘家强 on 16/7/29.
+//  Copyright © 2016年 刘家强. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppInfo : NSObject
+
+@end
---
+++ 7.29网络加载/AppInfo.m
@@ -0,0 +1,13 @@
+//
+//  AppInfo.m
+//  7.29网络加载
+//
+//  Created by 刘家强 on 16/7/29.
+//  Copyright © 2016年 刘家强. All rights reserved.
+//
+
+#import "AppInfo.h"
+
+@implementation AppInfo
+
+@end
